95874c74c5fbc22948869f93ab057e9d63702d8b
Henrique Pereira <pereiraehs@gmail.com>
2023-02-13 18:16:52 +0000

    create function for create new cleaned .csv files

--- demo/src/data-cleaning.R
+++ demo/src/data-cleaning.R
@@ -14,7 +14,17 @@ for(i in seq_along(file_list)){
 library(dplyr)
 
 # extract the stress and strain signls from each data frame
-# for my file I need to normalise length into strain, since my strain signal is corrupted during sample loading
 df_list_selected <- lapply(df_list, function(df) {
   return(select(df, one_of(c("V6", "V5"))))
 })
+
+# for my file I need to normalise length into strain, since my strain signal is corrupted during sample loading
+df_list_calculated <- lapply(df_list_selected, function(df) {
+  df$strain <- ((df$V5 - df$V5[1])/df$V5[1]) * 100
+  return(df)
+})
+
+# write each data frame to a separate CSV file
+for(i in seq_along(df_list_calculated)) {
+  write.csv(df_list_calculated[[i]], file = paste0("~/R/Biologic_tensile_properties/demo/input/", gsub(".dat", "", file_list[i]), "_cleaned.csv"), row.names= FALSE)
+}
